51480be7fbc9f92a894cc71abf689521436af99f
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2020-02-13 09:49:49 +0200

    Optimize Heat equation and add support for 2D

--- heat_equation.py
+++ heat_equation.py
@@ -12,13 +12,26 @@ class HeatEquation(object):
         self.dt = 0.1 * dx
         self.u = tf.constant(u)
 
-        def integrator(u):
-            f = tf.signal.fft(tf.cast(u, 'complex128'))
-            l = f.shape[-1]
-            ratio = (2*np.pi/(self.dx*l))**2
-            delta = self.dt * ratio
-            f *= tf.cast(tf.exp(-delta * tf.cast((l//2 - tf.abs(tf.range(l) - l//2))**2, 'float64')), 'complex128')
-            return tf.cast(tf.math.real(tf.signal.ifft(f)), 'float64')
+        l = u.shape[-1]
+        ratio = 2*np.pi / (l*self.dx)
+        delta = self.dt * ratio**2
+        omega2 = (l//2 - tf.abs(tf.range(l) - l//2))**2
+        dims = len(u.shape)
+        if dims == 1:
+            decay = tf.exp(-delta * tf.cast(omega2, 'complex128'))
+            def integrator(u):
+                f = tf.signal.fft(tf.cast(u, 'complex128'))
+                f *= decay
+                return tf.cast(tf.math.real(tf.signal.ifft(f)), 'float64')
+        elif dims == 2:
+            omega2_x, omega2_y = tf.meshgrid(omega2, omega2)
+            decay = tf.exp(-delta * tf.cast(omega2_x + omega2_y, 'complex128'))
+            def integrator(u):
+                f = tf.signal.fft2d(tf.cast(u, 'complex128'))
+                f *= decay
+                return tf.cast(tf.math.real(tf.signal.ifft2d(f)), 'float64')
+        else:
+            raise ValueError('Only up to 2D supported')
 
         self.integrator = tf.function(integrator)
 
@@ -36,19 +49,22 @@ if __name__ == '__main__':
 
     x = np.linspace(-5, 5, 256)
     dx = x[1] - x[0]
-    u = np.exp(-x*x) * bl_noise(x.shape)
+    x, y = np.meshgrid(x, x)
+    # u = np.exp(-x*x) * bl_noise(x.shape)
+    u = np.exp(-x*x -y*y) * bl_noise(x.shape)
 
     heat_equation = HeatEquation(u, dx)
 
     u = heat_equation.numpy()
-    plots = pylab.plot(x, u)
-
-    pylab.ylim(-1.0, 1.0)
+    plots = [pylab.imshow(u)]
+    # plots = pylab.plot(x, u)
+    # pylab.ylim(-1.0, 1.0)
 
     def update(frame):
         heat_equation.step()
-        v = heat_equation.numpy()
-        plots[0].set_ydata(v)
+        u = heat_equation.numpy()
+        plots[0].set_data(u)
+        # plots[0].set_ydata(u)
         return plots
 
     FuncAnimation(pylab.gcf(), update, frames=range(100), init_func=lambda: plots, blit=True, repeat=True, interval=20)
