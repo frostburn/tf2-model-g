bff8112d1dcea25825d1c142c5916c6aa8e7f316
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2020-02-13 18:36:03 +0200

    Add 3D support for Heat equation

--- heat_equation.py
+++ heat_equation.py
@@ -30,8 +30,15 @@ class HeatEquation(object):
                 f = tf.signal.fft2d(tf.cast(u, 'complex128'))
                 f *= decay
                 return tf.cast(tf.math.real(tf.signal.ifft2d(f)), 'float64')
+        elif dims == 3:
+            omega2_x, omega2_y, omega2_z = tf.meshgrid(omega2, omega2, omega2)
+            decay = tf.exp(-delta * tf.cast(omega2_x + omega2_y + omega2_z, 'complex128'))
+            def integrator(u):
+                f = tf.signal.fft3d(tf.cast(u, 'complex128'))
+                f *= decay
+                return tf.cast(tf.math.real(tf.signal.ifft3d(f)), 'float64')
         else:
-            raise ValueError('Only up to 2D supported')
+            raise ValueError('Only up to 3D supported')
 
         self.integrator = tf.function(integrator)
 
@@ -47,25 +54,25 @@ if __name__ == '__main__':
     import pylab
     from matplotlib.animation import FuncAnimation
 
-    x = np.linspace(-5, 5, 256)
+    x = np.linspace(-4, 4, 128)
     dx = x[1] - x[0]
-    x, y = np.meshgrid(x, x)
+    x, y, z = np.meshgrid(x, x, x)
     # u = np.exp(-x*x) * bl_noise(x.shape)
-    u = np.exp(-x*x -y*y) * bl_noise(x.shape)
+    u = np.exp(-x*x - y*y - z*z) * bl_noise(x.shape)
 
     heat_equation = HeatEquation(u, dx)
 
     u = heat_equation.numpy()
-    plots = [pylab.imshow(u)]
+    plots = [pylab.imshow(u[64])]
     # plots = pylab.plot(x, u)
     # pylab.ylim(-1.0, 1.0)
 
     def update(frame):
         heat_equation.step()
         u = heat_equation.numpy()
-        plots[0].set_data(u)
+        plots[0].set_data(u[64])
         # plots[0].set_ydata(u)
         return plots
 
-    FuncAnimation(pylab.gcf(), update, frames=range(100), init_func=lambda: plots, blit=True, repeat=True, interval=20)
+    FuncAnimation(pylab.gcf(), update, frames=range(100), init_func=lambda: plots, blit=True, repeat=True, interval=50)
     pylab.show()
