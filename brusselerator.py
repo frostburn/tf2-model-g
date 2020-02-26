8229d9750fde1d56e2945558fab39d0edba74f9a
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2020-02-26 10:45:14 +0200

    Implement a CAS derived integrator for the Brusselerator

--- brusselerator.py
+++ brusselerator.py
@@ -9,6 +9,22 @@ DEFAULT_PARAMS = {
 }
 
 
+def integrate_brusselerator_centered(a0, b0, t, A, B, k4):
+    a1 = a0**2*b0 + B*a0**2*k4/A + B*a0 + 2*A*a0*b0/k4 - a0*k4 + A**2*b0/k4**2
+    b1 = -a0**2*b0 - B*a0**2*k4/A - B*a0 - 2*A*a0*b0/k4 - A**2*b0/k4**2
+    a2 = a0*a1*b0 + 1/2*a0**2*b1 + B*a0*a1*k4/A + 1/2*B*a1 + A*a1*b0/k4 + A*a0*b1/k4 - 1/2*a1*k4 + 1/2*A**2*b1/k4**2
+    b2 = -a0*a1*b0 - 1/2*a0**2*b1 - B*a0*a1*k4/A - 1/2*B*a1 - A*a1*b0/k4 - A*a0*b1/k4 - 1/2*A**2*b1/k4**2
+    a3 = 1/3*a1**2*b0 + 2/3*a0*a2*b0 + 2/3*a0*a1*b1 + 1/3*a0**2*b2 + 1/3*B*a1**2*k4/A + 2/3*B*a0*a2*k4/A + 1/3*B*a2 + 2/3*A*a2*b0/k4 + 2/3*A*a1*b1/k4 + 2/3*A*a0*b2/k4 - 1/3*a2*k4 + 1/3*A**2*b2/k4**2
+    b3 = -1/3*a1**2*b0 - 2/3*a0*a2*b0 - 2/3*a0*a1*b1 - 1/3*a0**2*b2 - 1/3*B*a1**2*k4/A - 2/3*B*a0*a2*k4/A - 1/3*B*a2 - 2/3*A*a2*b0/k4 - 2/3*A*a1*b1/k4 - 2/3*A*a0*b2/k4 - 1/3*A**2*b2/k4**2
+    a4 = 1/2*a1*a2*b0 + 1/2*a0*a3*b0 + 1/4*a1**2*b1 + 1/2*a0*a2*b1 + 1/2*a0*a1*b2 + 1/4*a0**2*b3 + 1/2*B*a1*a2*k4/A + 1/2*B*a0*a3*k4/A + 1/4*B*a3 + 1/2*A*a3*b0/k4 + 1/2*A*a2*b1/k4 + 1/2*A*a1*b2/k4 + 1/2*A*a0*b3/k4 - 1/4*a3*k4 + 1/4*A**2*b3/k4**2
+    b4 = -1/2*a1*a2*b0 - 1/2*a0*a3*b0 - 1/4*a1**2*b1 - 1/2*a0*a2*b1 - 1/2*a0*a1*b2 - 1/4*a0**2*b3 - 1/2*B*a1*a2*k4/A - 1/2*B*a0*a3*k4/A - 1/4*B*a3 - 1/2*A*a3*b0/k4 - 1/2*A*a2*b1/k4 - 1/2*A*a1*b2/k4 - 1/2*A*a0*b3/k4 - 1/4*A**2*b3/k4**2
+
+    return (
+        a0 + t * (a1 + t * (a2 + t * (a3 + t*a4))),
+        b0 + t * (b1 + t * (b2 + t * (b3 + t*b4))),
+    )
+
+
 class Brusselerator(object):
     """
     Brusselerator is a simpler version of Model G that is either globally critical or sub-critical.
@@ -81,18 +97,20 @@ class Brusselerator(object):
         else:
             raise ValueError('Only up to 3D supported')
 
-        def reaction_integrator(con_X, con_Y):
-            new_X = con_X
-            new_Y = con_Y
-            for _ in range(self.fixed_point_iterations):
-                xy_flow = new_X*new_X*new_Y - self.params["B"] * new_X
-                v_X = self.params["A"] + xy_flow - self.params["k4"] * new_X
-                v_Y = -xy_flow
-                new_X = con_X + self.dt*v_X
-                new_Y = con_Y + self.dt*v_Y
-            con_X = new_X
-            con_Y = new_Y
-            return con_X, con_Y
+        # def reaction_integrator(con_X, con_Y):
+        #     new_X = con_X
+        #     new_Y = con_Y
+        #     for _ in range(self.fixed_point_iterations):
+        #         xy_flow = new_X*new_X*new_Y - self.params["B"] * new_X
+        #         v_X = self.params["A"] + xy_flow - self.params["k4"] * new_X
+        #         v_Y = -xy_flow
+        #         new_X = con_X + self.dt*v_X
+        #         new_Y = con_Y + self.dt*v_Y
+        #     con_X = new_X
+        #     con_Y = new_Y
+        #     return con_X, con_Y
+
+        reaction_integrator = lambda con_X, con_Y: integrate_brusselerator_centered(con_X, con_Y, self.dt, self.params['A'], self.params['B'], self.params['k4'])
 
         self.diffusion_integrator = tf.function(diffusion_integrator)
         self.reaction_integrator = tf.function(reaction_integrator)
@@ -121,18 +139,18 @@ if __name__ == '__main__':
     x, y = np.meshgrid(x, x)
 
     r2 = x*x+y*y
-    source_X = lambda t: np.exp(-(x-2)**2-y*y) / (t + 1)
+    source_X = lambda t: np.exp(-(x-4)**2-y*y) / (t + 1)
     zero = lambda t: 0
     source_functions = {
         'X': source_X,
         'Y': zero,
     }
-    brusselerator = Brusselerator(1.5 + np.exp(-0.2*r2)*bl_noise(x.shape), 1.5 + np.exp(-0.3*r2), dx, source_functions=source_functions)
+    brusselerator = Brusselerator(np.exp(-0.2*r2)*bl_noise(x.shape), np.exp(-0.3*r2), dx, source_functions=source_functions)
     X, Y = brusselerator.numpy()
     plots = []
     plots.extend(pylab.plot(x[0], X[64]))
     plots.extend(pylab.plot(x[0], Y[64]))
-    pylab.ylim(0.2, 3.2)
+    pylab.ylim(-1.5, 1.5)
 
     def update(frame):
         brusselerator.step()
